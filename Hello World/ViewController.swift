6146a28fafbe38c0d847e1c812277025cb006a34
Trent Stevens <trentstevens@Trents-MacBook-Pro.local>
2017-07-08 15:42:36 +1200

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Trent Stevens on 8/07/17.
+//  Copyright © 2017 Trent Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
